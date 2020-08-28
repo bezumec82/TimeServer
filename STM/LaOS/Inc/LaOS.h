9c6fa5e89bf0eb2f7fbad40afa5ae168e1f7b010
Константин Ласьков <laskov82@gmail.com>
2020-08-28 12:49:07 +0300

    LaOS to submodule

--- STM/LaOS/Inc/LaOS.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-/* This file provides abstraction from internal library names */
-
-#include "../Src/LaOSmain.h"
--- STM/LaOS/Inc/readme.txt
+++
@@ -1,2 +0,0 @@
-This folder contains the frontend of the solution.
-The user should add 'LaOS.h' in his code.
